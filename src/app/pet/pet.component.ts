12c5108997f0c4bbfee4cc16c01e9857a57ec98b
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-12-22 23:06:52 +0700

    [Thực hành] Chức năng thay đổi font-size

---
+++ src/app/pet/pet.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {Pet} from "../pet";
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.css']
+})
+export class PetComponent implements OnInit{
+  pet : Pet = {
+    name: 'Mạnh',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM-Ax2aCN-pQpDUiBtHtG10pERZKOkyrGgag&usqp=CAU'
+  };
+  imgSize = 400;
+
+  ngOnInit() {
+  }
+}
